38e8be2f5c27e47ba2b64e0c2369d1e76d5d592b
skim819 <skim819@gatech.edu>
2016-07-07 19:53:45 -0400

    Add files via upload

--- vpr2swcs/rasp30a_vmm.py
+++ vpr2swcs/rasp30a_vmm.py
@@ -160,9 +160,9 @@ class cab_vmmStats(stats):
         #li_sm_1 = ['vmm16x16.in[0:15]','vmm2x2.in[0:1]','cab_vmm.O[0:5]'] 
         
     # outputs order into the CAB
-        li_sm_0b = ['fgota[0:1].out[0]','ota_buf[0].out[0]','ota[0].out[0]','cap[0:3].out[0]','nfet[0:1].out[0]','pfet[0:1].out[0]','tgate[0:3].out[0]','nmirror_vmm[0:1].out[0]','ladder_blk[0].out[0:1]','c4_blk[0].out[0]','speech[0].out[0:1]','INFneuron[0].out[0]','lpf[0].out[0]','nfet_i2v[0].out[0]','pfet_i2v[0].out[0]','peak_detector[0].out[0]','ramp_fe[0].out[0]','sigma_delta_fe[0].out[0]','vmm_senseamp1[0].out[0]','vmm_senseamp2[0].out[0:1]','wta[0].out[0]','wta_primary[0].out[0:1]','common_source[0].out[0]','gnd_out[0].out[0]','vdd_out[0].out[0]','in2in_x1[0].out[0]','in2in_x6[0].out[0]','volt_div[0:1].out[0]','integrator[0].out[0]','integrator_nmirror[0].out[0]','tgate_so[0].out[0]','vmm4x4_SR[0].out[0]','vmm8x4_SR[0].out[0]','SR4[0].out[0:7]','vmm4x4_SR2[0].out[0]','vmm4x4[0].out[0:3]','sftreg[0].out[0]','DAC_sftreg[0].out[0]','sftreg2[0].out[0]','mmap_local_swc[0].out[0]','th_logic[0].out[0]','vmm8x4[0].out[0]','vmm8x4_in[0].out[0]','vmm12x1[0].out[0]','vmm12x1_wowta[0].out[0]','fg_io[0].out[0]','INV_cs[0].out[0]','dendrite_4x4[0].out[0:3]']
+        li_sm_0b = ['fgota[0:1].out[0]','ota_buf[0].out[0]','ota[0].out[0]','cap[0:3].out[0]','nfet[0:1].out[0]','pfet[0:1].out[0]','tgate[0:3].out[0]','nmirror_vmm[0:1].out[0]','ladder_blk[0].out[0:1]','c4_blk[0].out[0]','hhneuron[0].out[0:2]','speech[0].out[0:1]','INFneuron[0].out[0]','lpf[0].out[0]','nfet_i2v[0].out[0]','pfet_i2v[0].out[0]','peak_detector[0].out[0]','ramp_fe[0].out[0]','sigma_delta_fe[0].out[0]','vmm_senseamp1[0].out[0]','vmm_senseamp2[0].out[0:1]','wta[0].out[0]','wta_primary[0].out[0:1]','common_source[0].out[0]','gnd_out[0].out[0]','vdd_out[0].out[0]','in2in_x1[0].out[0]','in2in_x6[0].out[0]','volt_div[0:1].out[0]','integrator[0].out[0]','integrator_nmirror[0].out[0]','tgate_so[0].out[0]','vmm4x4_SR[0].out[0]','vmm8x4_SR[0].out[0]','SR4[0].out[0:7]','vmm4x4_SR2[0].out[0]','vmm4x4[0].out[0:3]','sftreg[0].out[0]','DAC_sftreg[0].out[0]','ladder_filter[0].out[0:2]','sftreg2[0].out[0]','mmap_local_swc[0].out[0]','th_logic[0].out[0]','vmm8x4[0].out[0]','vmm8x4_in[0].out[0]','vmm12x1[0].out[0]','vmm12x1_wowta[0].out[0]','fg_io[0].out[0]','INV_cs[0].out[0]','dendrite_4x4[0].out[0:3]']
     # defining the inputs order into the CAB #?? last term to connect i/ps to o/ps?
-        li_sm_1 = ['fgota[0:1].in[0:1]','ota_buf[0].in[0]','ota[0].in[0:1]','cap[0:3].in[0]','nfet[0:1].in[0:1]','pfet[0:1].in[0:1]','tgate[0:3].in[0:1]','nmirror_vmm[0:1].in[0]','ladder_blk[0].in[0:1]','c4_blk[0].in[0:1]','speech[0].in[0:2]','INFneuron[0].in[0:2]','lpf[0].in[0]','nfet_i2v[0].in[0]','pfet_i2v[0].in[0]','peak_detector[0].in[0:1]','ramp_fe[0].in[0]','sigma_delta_fe[0].in[0:4]','vmm_senseamp1[0].in[0:1]','vmm_senseamp2[0].in[0:3]','wta[0].in[0:1]','wta_primary[0].in[0]','common_source[0].in[0]','gnd_out[0].in[0:1]','vdd_out[0].in[0:1]','in2in_x1[0].in[0:2]','in2in_x6[0].in[0:12]','volt_div[0:1].in[0:1]','integrator[0].in[0:2]','integrator_nmirror[0].in[0:2]','tgate_so[0].in[0:7]','vmm4x4_SR[0].in[0:6]','vmm8x4_SR[0].in[0:10]','SR4[0].in[0:3]','vmm4x4_SR2[0].in[0:7]','vmm4x4[0].in[0:3]','sftreg[0].in[0:18]','DAC_sftreg[0].in[0:2]','sftreg2[0].in[0:2]','mmap_local_swc[0].in[0:2]','th_logic[0].in[0:7]','vmm8x4[0].in[0:12]','vmm8x4_in[0].in[0:12]','vmm12x1[0].in[0:12]','vmm12x1_wowta[0].in[0:11]','fg_io[0].in[0]','INV_cs[0].in[0:1]','dendrite_4x4[0].in[0:3]','cab_vmm.O[0:7]'] 
+        li_sm_1 = ['fgota[0:1].in[0:1]','ota_buf[0].in[0]','ota[0].in[0:1]','cap[0:3].in[0]','nfet[0:1].in[0:1]','pfet[0:1].in[0:1]','tgate[0:3].in[0:1]','nmirror_vmm[0:1].in[0]','ladder_blk[0].in[0:1]','c4_blk[0].in[0:1]','hhneuron[0].in[0:3]','speech[0].in[0:2]','INFneuron[0].in[0:2]','lpf[0].in[0]','nfet_i2v[0].in[0]','pfet_i2v[0].in[0]','peak_detector[0].in[0:1]','ramp_fe[0].in[0]','sigma_delta_fe[0].in[0:4]','vmm_senseamp1[0].in[0:1]','vmm_senseamp2[0].in[0:3]','wta[0].in[0:1]','wta_primary[0].in[0]','common_source[0].in[0]','gnd_out[0].in[0:1]','vdd_out[0].in[0:1]','in2in_x1[0].in[0:2]','in2in_x6[0].in[0:12]','volt_div[0:1].in[0:1]','integrator[0].in[0:2]','integrator_nmirror[0].in[0:2]','tgate_so[0].in[0:7]','vmm4x4_SR[0].in[0:6]','vmm8x4_SR[0].in[0:10]','SR4[0].in[0:3]','vmm4x4_SR2[0].in[0:7]','vmm4x4[0].in[0:3]','sftreg[0].in[0:18]','DAC_sftreg[0].in[0:2]','ladder_filter[0].in[0:1]','sftreg2[0].in[0:2]','mmap_local_swc[0].in[0:2]','th_logic[0].in[0:7]','vmm8x4[0].in[0:12]','vmm8x4_in[0].in[0:12]','vmm12x1[0].in[0:12]','vmm12x1_wowta[0].in[0:11]','fg_io[0].in[0]','INV_cs[0].in[0:1]','dendrite_4x4[0].in[0:3]','cab_vmm.O[0:7]'] 
 	    
 	          
         li_sm = ['gnd'             	,[0,  0],     #inputs from CAB and device outputs
@@ -173,6 +173,7 @@ class cab_vmmStats(stats):
 		'fgota[0:1].out[0]'  	,[0, range(15, 17)],#y
 		'ota_buf[0].out[0]'  	,[0, 17],#y
 		'ota[0].out[0]'      	,[0, 18],#y
+                'ladder_filter[0].out[0:2]'      , [0, [15,16,18]],#y
 		'cap[0:3].out[0]'    	,[0, range(19, 23)],#y                                
 		'nfet[0:1].out[0]'   	,[0, range(24, 22, -1)],#y numbering chnge for nFET0(24) and nFET1(23), needs to be verified
 		'pfet[0:1].out[0]'   	,[0, range(26, 24,-1)],#y numbering chnge for pFETt0(26) and pFET1(23)
@@ -182,6 +183,7 @@ class cab_vmmStats(stats):
 		'nmirror_vmm[0:1].out[0]'	,[0, range(31, 33)],#y
 		'ladder_blk[0].out[0:1]',[0,[17,18]],
 		'c4_blk[0].out[0]'	 ,[0,15],
+	   	 'hhneuron[0].out[0:2]',[0,[18,15,16]],#Vmem,VNa,VK
     	        'speech[0].out[0:1]'	 ,[0,[17,26]],
 		'lpf[0].out[0]'  	 ,[0, 17],
                 'nfet_i2v[0].out[0]',[0, 2+15],
@@ -222,6 +224,7 @@ class cab_vmmStats(stats):
 		'fgota[0:1].in[0:1]' ,[range(33,29,-1), 0],# in<0:7> y its high because of the decoded address where 4==33 for 
 		'ota_buf[0].in[0]' ,  [29, 0],# in<0:7> y
 		'ota[0].in[0:1]'     ,[range(27,25,-1), 0],# in<0:7> y
+                'ladder_filter[0].in[0:1]'      , [[33,30],0],#y
 		'cap[0:3].in[0]'     ,[range(25,21,-1), 0],# in<8:11 y
 		'nfet[0:1].in[0:1]'  ,[[19, 18, 21, 20], 0],# in<12:15> y 21, 17,-1) it's flipped
 		'pfet[0:1].in[0:1]'  ,[[15, 14, 17, 16], 0],# in<16:19> n---change (17, 13,-1) it;s flipped
@@ -231,6 +234,7 @@ class cab_vmmStats(stats):
 		'nmirror_vmm[0:1].in[0]' ,[range(5,3,-1), 0],# in<28:29> y
 		'ladder_blk[0].in[0:1]',[[29,26],0],
 		'c4_blk[0].in[0:1]'   ,[[33,25],0],
+	        'hhneuron[0].in[0:3]',[[25,16,26,33],0],#Vin,ENa,EK,Vref27
 		'speech[0].in[0:2]'   ,[[33,25,14],0],#25
 		'lpf[0].in[0]'	  	,[29,0],
                 'nfet_i2v[0].in[0]',[29,0],
@@ -274,8 +278,8 @@ class cab_vmmStats(stats):
         self.li1 = recStrExpand(li_sm_1)
         #pdb.set_trace()
         #CAB Devices ## order is very important here
-	self.dev_types =['fgota']*2 + ['ota_buf']*1 + ['ota']*1 + ['cap']*4+ ['nfet']*2 + ['pfet']*2  + ['tgate']*4 + ['nmirror_vmm']*2+['ladder_blk']*1+ ['c4_blk']*1+ ['speech']*1+ ['INFneuron']*1+ ['lpf']*1+['nfet_i2v']*1+['pfet_i2v']*1+['peak_detector']*1+['ramp_fe']*1+['sigma_delta_fe']*1+['vmm_senseamp1']*1+['vmm_senseamp2']*1+['wta']*1+['wta_primary']*1+['common_source']*1+['gnd_out']*1+['vdd_out']*1+['in2in_x1']*1+['in2in_x6']*1+['volt_div']*2+['integrator']*1+['integrator_nmirror']*1+['tgate_so']*1+['vmm4x4_SR']*1+['vmm8x4_SR']*1+['SR4']*1+['vmm4x4_SR2']*1+['vmm4x4']*1+['sftreg']*1+['DAC_sftreg']*1 +['sftreg2']*1+['mmap_local_swc']*1+['th_logic']*1+['vmm8x4']*1+['vmm8x4_in']*1+['vmm12x1']*1+['vmm12x1_wowta']*1+['fg_io']*1+['INV_cs']*1+['dendrite_4x4']*1
-	self.dev_pins = {'fgota_in':2,'ota_buf_in':1,'ota_in':2, 'cap_in':1, 'nfet_in':2, 'pfet_in':2,'tgate_in':2,'nmirror_vmm_in':1,'ladder_blk_in':2, 'c4_blk_in':2,'speech_in':3,'INFneuron_in':3,'lpf_in':1,'nfet_i2v_in':1,'pfet_i2v_in':1,'peak_detector_in':2,'ramp_fe_in':1,'sigma_delta_fe_in':5,'vmm_senseamp1_in':2,'vmm_senseamp2_in':4,'wta_in':2,'wta_primary_in':1,'common_source_in':1,'gnd_out_in':2,'vdd_out_in':2,'in2in_x1_in':3,'in2in_x6_in':13,'volt_div_in':2,'integrator_in':3,'integrator_nmirror_in':3,'tgate_so_in':8,'vmm4x4_SR_in':7,'vmm8x4_SR_in':11,'SR4_in':4,'vmm4x4_SR2_in':8,'vmm4x4_in':4,'vmm12x1_in':13,'vmm12x1_wowta_in':12,'fg_io_in':1,'INV_cs_in':2,'dendrite_4x4_in':4,'fgota_out':1,'ota_buf_out':1,'ota_out':1, 'cap_out':1, 'nfet_out':1, 'pfet_out':1,'tgate_out':1,'nmirror_vmm_out':1,'ladder_blk_out':2, 'c4_blk_out':1,'speech_out':2,'INFneuron_out':1,'lpf_out':1,'nfet_i2v_out':1,'pfet_i2v_out':1,'peak_detector_out':1,'ramp_fe_out':1,'sigma_delta_fe_out':1,'vmm_senseamp1_out':1,'vmm_senseamp2_out':2,'wta_out':1,'wta_primary_out':2,'common_source_out':1,'gnd_out_out':1,'vdd_out_out':1,'in2in_x1_out':1,'in2in_x6_out':1,'volt_div_out':1,'integrator_out':1,'integrator_nmirror_out':1,'tgate_so_out':1,'vmm4x4_SR_out':1,'vmm8x4_SR_out':1,'SR4_out':8,'vmm4x4_SR2_out':1,'vmm4x4_out':4, 'sftreg_in':19, 'DAC_sftreg_in':3, 'sftreg_out':1,'DAC_sftreg_out':1, 'sftreg2_in':3, 'sftreg2_out':1,'mmap_local_swc_in':3, 'mmap_local_swc_out':1,'th_logic_in':8, 'th_logic_out':1,'vmm8x4_in':13,'vmm8x4_out':1,'vmm8x4_in_in':13,'vmm8x4_in_out':1,'vmm12x1_out':1,'vmm12x1_wowta_out':1,'fg_io_out':1,'INV_cs_out':1,'dendrite_4x4_out':4}  
+	self.dev_types =['fgota']*2 + ['ota_buf']*1 + ['ota']*1 + ['cap']*4+ ['nfet']*2 + ['pfet']*2  + ['tgate']*4 + ['nmirror_vmm']*2+['ladder_blk']*1+ ['c4_blk']*1+['hhneuron']*1+ ['speech']*1+ ['INFneuron']*1+ ['lpf']*1+['nfet_i2v']*1+['pfet_i2v']*1+['peak_detector']*1+['ramp_fe']*1+['sigma_delta_fe']*1+['vmm_senseamp1']*1+['vmm_senseamp2']*1+['wta']*1+['wta_primary']*1+['common_source']*1+['gnd_out']*1+['vdd_out']*1+['in2in_x1']*1+['in2in_x6']*1+['volt_div']*2+['integrator']*1+['integrator_nmirror']*1+['tgate_so']*1+['vmm4x4_SR']*1+['vmm8x4_SR']*1+['SR4']*1+['vmm4x4_SR2']*1+['vmm4x4']*1+['sftreg']*1+['DAC_sftreg']*1  +['sftreg2']*1+['mmap_local_swc']*1+['th_logic']*1+['vmm8x4']*1+['vmm8x4_in']*1+['vmm12x1']*1+['vmm12x1_wowta']*1+['fg_io']*1+['INV_cs']*1+['dendrite_4x4']*1+['ladder_filter']*1
+	self.dev_pins = {'fgota_in':2,'ota_buf_in':1,'ota_in':2, 'cap_in':1, 'nfet_in':2, 'pfet_in':2,'tgate_in':2,'nmirror_vmm_in':1,'ladder_blk_in':2, 'c4_blk_in':2,'hhneuron_in':4,'speech_in':3,'INFneuron_in':3,'lpf_in':1,'nfet_i2v_in':1,'pfet_i2v_in':1,'peak_detector_in':2,'ramp_fe_in':1,'sigma_delta_fe_in':5,'vmm_senseamp1_in':2,'vmm_senseamp2_in':4,'wta_in':2,'wta_primary_in':1,'common_source_in':1,'gnd_out_in':2,'vdd_out_in':2,'in2in_x1_in':3,'in2in_x6_in':13,'volt_div_in':2,'integrator_in':3,'integrator_nmirror_in':3,'tgate_so_in':8,'vmm4x4_SR_in':7,'vmm8x4_SR_in':11,'SR4_in':4,'vmm4x4_SR2_in':8,'vmm4x4_in':4,'vmm12x1_in':13,'vmm12x1_wowta_in':12,'fg_io_in':1,'INV_cs_in':2,'dendrite_4x4_in':4,'ladder_filter_in':2,'fgota_out':1,'ota_buf_out':1,'ota_out':1, 'cap_out':1, 'nfet_out':1, 'pfet_out':1,'tgate_out':1,'nmirror_vmm_out':1,'ladder_blk_out':2, 'c4_blk_out':1,'hhneuron_out':3,'speech_out':2,'INFneuron_out':1,'lpf_out':1,'nfet_i2v_out':1,'pfet_i2v_out':1,'peak_detector_out':1,'ramp_fe_out':1,'sigma_delta_fe_out':1,'vmm_senseamp1_out':1,'vmm_senseamp2_out':2,'wta_out':1,'wta_primary_out':2,'common_source_out':1,'gnd_out_out':1,'vdd_out_out':1,'in2in_x1_out':1,'in2in_x6_out':1,'volt_div_out':1,'integrator_out':1,'integrator_nmirror_out':1,'tgate_so_out':1,'vmm4x4_SR_out':1,'vmm8x4_SR_out':1,'SR4_out':8,'vmm4x4_SR2_out':1,'vmm4x4_out':4, 'sftreg_in':19, 'DAC_sftreg_in':3, 'sftreg_out':1,'DAC_sftreg_out':1, 'sftreg2_in':3, 'sftreg2_out':1,'mmap_local_swc_in':3, 'mmap_local_swc_out':1,'th_logic_in':8, 'th_logic_out':1,'vmm8x4_in':13,'vmm8x4_out':1,'vmm8x4_in_in':13,'vmm8x4_in_out':1,'vmm12x1_out':1,'vmm12x1_wowta_out':1,'fg_io_out':1,'INV_cs_out':1,'dendrite_4x4_out':4,'ladder_filter_out':3}  
         
         dev_fgs_sm = ['fgota[0:1]',[0, [58, 60]],## block names
 		'ota_buf[0]' 	,[0, 62],
@@ -287,6 +291,7 @@ class cab_vmmStats(stats):
 	        'ota[0]'	,[0, 63],
 		'ladder_blk[0]' ,[0,18],
 		'c4_blk[0]'  	, [0,62], # set as ota[0] now
+		'hhneuron[0]' ,[0,0],
 		'speech[0]'  	, [0,62], 
 		'INFneuron[0]' 	, [0,62], # set as ota[0] now
 		'peak_detector[0]' , [0,62],
@@ -317,6 +322,7 @@ class cab_vmmStats(stats):
 		'SR4[0]'	,[0,0],
 		'sftreg[0]' , [0,0],
 		'DAC_sftreg[0]' , [0,0],
+		'ladder_filter[0]'  	, [0,62],
 		'nmirror_vmm[0]',[0,0],
 		'sftreg2[0]' , [0,0],
 		'mmap_local_swc[0]' , [0,0],
@@ -344,6 +350,15 @@ class cab_vmmStats(stats):
            	'c4_ota_n_bias[0]', [[33, 0-62+58]],
 		'c4_ota_p_bias[1]', [[33, 1-62+60]],
            	'c4_ota_n_bias[1]', [[33, 0-62+60]], 
+		'hh_nabias', [32,58],
+		'hh_kbias', [32,60],
+		'hh_fbpfetbias', [32,15],
+		'hh_vinbias', [32,63],
+		'hh_ota_p_bias[0]', [[33, 59]],
+        	'hh_ota_n_bias[0]', [[33, 58]],
+		'hh_ota_p_bias[1]', [[33, 61]],
+        	'hh_ota_n_bias[1]', [[33, 60]],
+		'hh_leak', [31,1],
              	'speech_fg[0]',[[32,19-62],[32,20-62],[24,15-62],[28,62-62],[32,16-62],[30,16-62],[31,15-62],[31,1-62+58],[31,0-62+58],[31,1-62+60],[31,0-62+60],[28,2-62+57],[28,1-62+57],[28,0-62+57],[26,25-62],[27,15-62],[17,18-62],[16,0-62],[29,25-62],[28,17-62],[15,17-62]], #[16,0-62],[21,26-62],,[15,25-62],[14,1-62],pfet[15,17],[0,26]
 		        'speech_pfetbias[0]' ,[[29,1-62]], #[26,1-62]
 		         'speech_pfetbias[1]' ,[[29,0-62]], #[26,1-62]
@@ -361,7 +376,10 @@ class cab_vmmStats(stats):
 		'INV_cs_otaPBIAS[0]',[33,59],
 		'INV_cs_otaNBIAS[0]',[33,58],
 		'peak_ota_bias[0]',[[32, 0-62+58]],
+           	'ladder_fg_fb[0]', [[30,16-62]],
+           	'ladder_filter_fg[0]', [[32,16-62],[27,16-62],[26,18-62],[31,15-62],[31,1-62+58],[31,0-62+58],[31,1-62+60],[31,0-62+60]],
 		'c4_fg[0]'	,[[32,-43],[32,-46],[30,-46],[31,-47]],#[ota_bias0|ota0neg->cap0|cap0.in->ota0out|ota_bias1|ota1_fb] |c2 and c3 conections
+		'hhneuron_fg[0]', [[32,19],[25,20],[24,19],[31,58],[31,59],[28,2+60],[28,1+60],[28,0+60],[28,2+57],[28,1+57],[28,0+57],[17,15],[25,25],[25,24],[31,24],[30,16],[19,16],[25,18],[31,60],[31,61],[26,27],[18,27]],
 		'c4_cap_3x[0]',[28,2-62+57],
 		'c4_cap_2x[0]',[28,1-62+57],
 		'c4_cap_1x[0]',[28,0-62+57],
@@ -596,7 +614,7 @@ class cab_vmm(complexBlock):
 		    #pdb.set_trace()
 		    for s in range(len(ex_fg)):
 			    for j in ex_fg[s].split()[::2]:
-				if swc_name0 in["ladder_blk[0]","c4_blk[0]","speech[0]","INFneuron[0]","lpf[0]","peak_detector[0]","ramp_fe[0]",'sigma_delta_fe[0]','nmirror_vmm[0]']:
+				if swc_name0 in["ladder_blk[0]","hhneuron[0]","c4_blk[0]","speech[0]","INFneuron[0]","lpf[0]","peak_detector[0]","ramp_fe[0]",'sigma_delta_fe[0]','nmirror_vmm[0]']:
 					#pdb.set_trace()
 					swc_name1 = j
 				#elif 
@@ -680,6 +698,10 @@ class cab_vmm(complexBlock):
 						swcx.append(3)	
 					elif swc_name1 in ['c4_ota_bias[0]','c4_ota_bias[1]','peak_ota_bias[0]','wta_bufbias[0]','wta_primary_bufbias[0]','speech_peakotabias[0]','speech_peakotabias[1]','vmm12x1_otabias[0]','nfet_i2v_otabias[0]','pfet_i2v_otabias[0]','th_logic_otabias[0]','INV_cs_otaBIAS[0]']:
 						swcx.append(2)
+					elif swc_name1 in ['hh_ota_p_bias[0]','hh_ota_n_bias[0]','hh_ota_p_bias[1]','hh_ota_n_bias[1]']:
+						swcx.append(3)
+					elif swc_name1 in ['hh_nabias','hh_kbias','hh_vinbias']:
+						swcx.append(2)
 					elif ex_fg[s].split('=')[0] in ['ota0bias ','ota1bias ','fgota0bias ','fgota1bias ','integrator_ota0 ','integrator_ota1 ','integrator_nmirror_ota0 ','integrator_nmirror_ota1 ','fgota_biasfb[0] ','fgota_biasfb[1] ','fgota_biasfb '] and n==0:
 					        #pdb.set_trace()
 						swcx.append(2)
@@ -687,7 +709,7 @@ class cab_vmm(complexBlock):
 						swcx.append(2)
 					elif swc_name1 == 'fgota_biasfb[1] 'and n==0:
 						swcx.append(2)
-					elif swc_name1 in ['vd_target[0]','vd_target[1]','ramp_pfetinput[0]','ramp_pfetinput[1]','wta_pfetbias[0]','wta_primary_pfetbias1[0]','wta_primary_pfetbias[0]','speech_pfetbias[0]','speech_pfetbias[1]','vmm12x1_pfetbias[0]','integrator_nmirror_ota2 ','DAC_bias_pfet[0]','vmm12x1_offsetbias[0]','fg_io_fg[0]','nmirror_bias[0]','INF_bias[0]','cs_bias[0]','INV_cs_NBIAS[0]','INV_cs_PBIAS[0]']:
+					elif swc_name1 in ['hh_leak','hh_fbpfetbias','vd_target[0]','vd_target[1]','ramp_pfetinput[0]','ramp_pfetinput[1]','wta_pfetbias[0]','wta_primary_pfetbias1[0]','wta_primary_pfetbias[0]','speech_pfetbias[0]','speech_pfetbias[1]','vmm12x1_pfetbias[0]','integrator_nmirror_ota2 ','DAC_bias_pfet[0]','vmm12x1_offsetbias[0]','fg_io_fg[0]','nmirror_bias[0]','INF_bias[0]','cs_bias[0]','INV_cs_NBIAS[0]','INV_cs_PBIAS[0]']:
 						swcx.append(1)
 					elif swc_name1 in ['INF_bias[0]']:
 						swcx.append(1)
